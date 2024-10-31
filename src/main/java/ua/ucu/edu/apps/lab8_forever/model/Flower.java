0e0ea9c091dc8afd344e1d4c0ed616ed1a0754b1
Yaroslav Kornetskyi <kornetskyi.pn@ucu.edu.ua>
2024-10-31 13:10:37 +0200

    Updated to reboot version.

--- src/main/java/ua/ucu/edu/apps/lab8_forever/model/Flower.java
+++ src/main/java/ua/ucu/edu/apps/lab8_forever/model/Flower.java
@@ -3,10 +3,12 @@ package ua.ucu.edu.apps.lab8_forever.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter @Setter
 @Entity
 @Table(name = "flowers")
+@ToString
 public class Flower extends Item{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
